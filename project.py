6b24c0c2cf4466988f4edeffc0239814d3e8ef90
Nicol Narciso <nicol.gaspar.narciso@gmail.com>
2019-05-27 20:26:10 +0200

    feat: Write result to file implemented

---
+++ project.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+
+db_connection = None
+db_cursor = None
+
+
+
+def report1():
+    result = ''
+    result += '1. What are the most popular three articles of all time?\n'
+    result += '   Which articles have been accessed the most?\n'
+    result += '   Present this information as a sorted list\n'
+    result += '   with the most popular article at the top.\n'
+    result += '\n'
+
+    print(result)
+    return result
+
+def report2():
+    result = ''
+    result += '2. Who are the most popular article authors of all time?\n'
+    result += '   That is, when you sum up all of the articles each author\n'
+    result += '   has written, which authors get the most page views?\n'
+    result += '   Present this as a sorted list with the most popular author\n'
+    result += '   at the top.\n'
+    result += '\n'
+
+    print(result)
+    return result
+
+def report3():
+    result = ''
+    result += '3. On which days did more than 1% of requests lead to errors?\n'
+    result += '   The log table includes a column status that indicates the HTTP\n'
+    result += '   status code that the news site sent to the user\'s browser.\n'
+    result += '\n'
+
+    print(result)
+    return result
+
+
+
+
+if __name__ == '__main__':
+    db_connection = psycopg2.connect(database='news')
+    db_cursor = db_connection.cursor()
+
+    with open('result.txt', 'w') as resultfile:
+        resultfile.writelines(report1())
+        resultfile.writelines(report2())
+        resultfile.writelines(report3())
+
+    db_connection.close()
