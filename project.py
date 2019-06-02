47aeacf27ad17c6af968cd85fe471f7ecc765256
Nicol Narciso <nicol.gaspar.narciso@gmail.com>
2019-06-02 21:09:33 +0200

    feat: Second and third query implemented

--- project.py
+++ project.py
@@ -21,6 +21,7 @@ def report1(cursor):
     print(result)
     return result
 
+
 def report2(cursor):
     result = ''
     result += '2. Who are the most popular article authors of all time?\n'
@@ -28,31 +29,46 @@ def report2(cursor):
     result += '   has written, which authors get the most page views?\n'
     result += '   Present this as a sorted list with the most popular author\n'
     result += '   at the top.\n'
+    cursor.execute(('SELECT authors.name, count(log.id) as views '
+                    'FROM authors, articles, log '
+                    'WHERE articles.author = authors.id '
+                    'AND log.path LIKE \'%\' || articles.slug || \'%\' '
+                    'GROUP BY authors.name '
+                    'ORDER BY views DESC '
+                    'LIMIT 3;'))
+    for idx, item in enumerate(cursor.fetchall()):
+        result += '      {}. {:35} {}views\n'.format(idx+1, item[0], item[1])
     result += '\n'
-
     print(result)
     return result
+
 
 def report3(cursor):
     result = ''
     result += '3. On which days did more than 1% of requests lead to errors?\n'
-    result += '   The log table includes a column status that indicates the HTTP\n'
-    result += '   status code that the news site sent to the user\'s browser.\n'
+    result += '   The log table includes a column status that indicates'
+    result += ' the HTTP\n'
+    result += '   status code that the news site sent to the user\'s'
+    result += ' browser.\n'
+    cursor.execute(('SELECT to_char(time,\'DD-MON-YYYY\') as day, '
+                    'count(*) as count_errors '
+                    'FROM log '
+                    'WHERE STATUS = \'404 NOT FOUND\' '
+                    'GROUP BY day '
+                    'ORDER BY count_errors DESC '
+                    'LIMIT 1;'))
+    for idx, item in enumerate(cursor.fetchall()):
+        result += '      {}. {:35} {}errors\n'.format(idx+1, item[0], item[1])
     result += '\n'
-
     print(result)
     return result
-
-
 
 
 if __name__ == '__main__':
     db_connection = psycopg2.connect(database='news')
     db_cursor = db_connection.cursor()
-
     with open('result.txt', 'w') as resultfile:
         resultfile.writelines(report1(cursor=db_cursor))
         resultfile.writelines(report2(cursor=db_cursor))
         resultfile.writelines(report3(cursor=db_cursor))
-
     db_connection.close()
